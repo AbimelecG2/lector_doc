e260b1de1b93f420c166ae99c27f68fdff28c3bb
Abimelec <abimelecg2@unitec.edu>
2023-12-08 15:36:05 -0600

    sigo intentndo

--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:lector_doc/main.dart';
 
 class ResultScreen extends StatefulWidget {
   final String text;
@@ -11,30 +11,93 @@ class ResultScreen extends StatefulWidget {
 }
 
 class _ResultScreenState extends State<ResultScreen> {
-  List<String> results = [];
+  List<Map<String, String>> results = [];
 
   @override
   void initState() {
     super.initState();
-    results.add(widget.text);
+    results.add({"title": "Scan 1", "text": widget.text});
+  }
+
+  Future<void> _editTitle(int index) async {
+    TextEditingController _titleController = TextEditingController();
+    _titleController.text = results[index]["title"] ?? "";
+
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Edit Title'),
+          content: TextField(
+            controller: _titleController,
+            decoration: InputDecoration(
+              hintText: 'Enter new title',
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Save'),
+              onPressed: () {
+                setState(() {
+                  results[index]["title"] = _titleController.text;
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Resultados Escaneados'),
+        title: const Text('Scan Results'),
       ),
       body: ListView.builder(
         itemCount: results.length,
         itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-              title: Text('Resultado ${index + 1}'),
-              subtitle: Text(results[index]),
+          var result = results[index];
+          return Container(
+            margin: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 1,
+                  blurRadius: 5,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Card(
+              child: ListTile(
+                title: Text(result["title"] ?? "Untitled"),
+                subtitle: Text(result["text"] ?? ""),
+                trailing: IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () => _editTitle(index),
+                ),
+              ),
             ),
           );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Agregar lógica para escanear más texto y añadirlo a los resultados
+          MainScreen();
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
